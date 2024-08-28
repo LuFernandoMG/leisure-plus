e81bd885d74184f18e2b9519f516a209e7121aa6
Luis Fernando Méndez <luisfernando.mendez5@gmail.com>
2024-08-27 23:53:07 -0300

    feat: featured and poster components first approach.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,28 @@ import { fetchData } from "@/utils/fetchData";
 import Carousel from "@/components/Carousel";
 import Hero from "@/components/Hero";
 import styles from "./page.module.scss";
+import Featured from "@/components/Featured";
+import Poster from "@/components/Poster";
 
 export default async function Home() {
-  let popularMovies = await fetchData({ endpoint: "/movie/popular"});
-  let popularSeries = await fetchData({ endpoint: "/tv/popular"});
-  let topRatedMovies = await fetchData({ endpoint: "/movie/top_rated"});
-  let topRatedSeries = await fetchData({ endpoint: "/tv/top_rated"});
-  let heroMovie = await fetchData({ endpoint: "/movie/592831"});
+  const popularMovies = await fetchData({ endpoint: "/movie/popular"});
+  const popularSeries = await fetchData({ endpoint: "/tv/popular"});
+  const topRatedMovies = await fetchData({ endpoint: "/movie/top_rated"});
+  const topRatedSeries = await fetchData({ endpoint: "/tv/top_rated"});
+  const heroMovie = await fetchData({ endpoint: "/movie/592831"});
+  const featuredShowHOTD = await fetchData({ endpoint: "/tv/94997"}); 
+  const featuredShowGOT = await fetchData({ endpoint: "/tv/1399"});
 
+  console.log('featuredShowHOTD', featuredShowHOTD);
   return (
     <main className={styles.main}>
       <div className={styles.movie_container}>
         <Hero movie={heroMovie} />
         <Carousel movies={popularMovies.results} title="Popular movies" />
+        <Featured videoUrl="https://www.youtube.com/embed/YN2H_sKcmGw?si=r2v1M_pZpbk-QsN_" switchSides>
+          <Poster element={featuredShowGOT} />
+          <Poster element={featuredShowHOTD} />
+        </Featured>
         <Carousel movies={popularSeries.results} title="Popular TV Shows" />
         <Carousel movies={topRatedSeries.results} title="Top rated TV Shows" />
         <Carousel movies={topRatedMovies.results} title="Top rated Movies" />
