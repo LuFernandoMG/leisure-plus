322a724904e2a98278626274546d9e3289f3ba8d
Luis Fernando Méndez <luisfernando.mendez5@gmail.com>
2024-09-01 23:14:59 -0300

    feat: add E2E test first mockup

---
+++ cypress/e2e/goToMoviesAndSortGrid.cy.ts
@@ -0,0 +1,18 @@
+describe('General E2E Test', () => {
+  it('Go to Movies', () => {
+    cy.visit('localhost:3000')
+    cy.contains('Movies').click()
+
+    // Check if we are in the movies page
+    cy.url().should('include', '/movies')
+
+    // Check if the movies are sorted by genres and change to Grid view
+    cy.get('checkbox').click()
+
+    // Check if the movies are displayed in a grid view
+    cy.get('movies__grid').should('exist')
+
+    // Sort the movies by popularity
+    cy.get('select').select('popularity.desc')
+  })
+})
